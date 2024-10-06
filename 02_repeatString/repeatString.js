6398e745bb9be4bc9445a805d39f1d64b4414a74
Sam Hunt <sam@poggle.org>
2024-10-06 21:10:05 +0100

    Complete excercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeats) {
+    let output = "";
+    if (repeats < 0) {
+        output = "ERROR"
+    } else {
+        for (let i = 1; i <= repeats; i++) {
+            output = output + text;
+        }
+    }
+    return output
 };
 
 // Do not edit below this line
