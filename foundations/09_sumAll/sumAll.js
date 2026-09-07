e199cbb6a7080f9b7b9484f3353ef35a4fa7b956
Sam Hunt <sam@poggle.org>
2026-09-07 20:44:31 +0100

    Complete Exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let total = 0;
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+    if (num1 > num2){
+        let temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
+    while (num2 >= num1){
+        total += num2;
+        num2 -= 1;
+    }
+    return total;
 };
 
 // Do not edit below this line
