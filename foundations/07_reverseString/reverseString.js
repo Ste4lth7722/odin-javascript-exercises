f2e170dccde6555dfbdb33ff4acdca0b7ea161b8
Sam Hunt <sam@poggle.org>
2026-09-07 20:25:56 +0100

    Complete exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversed = [];
+    for (let i = 0; i < str.length; i++){
+        reversed[str.length - (i + 1)] = str[i];
+    }
+    return reversed.join('');
 };
 
 // Do not edit below this line
