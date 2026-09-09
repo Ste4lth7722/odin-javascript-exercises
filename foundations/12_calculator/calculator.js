24603fc34fdbd35f27f91dd07dcad2fbd9ee5905
Sam Hunt <sam@poggle.org>
2026-09-09 21:31:58 +0100

    Complete exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, curr) => {
+    total += curr;
+    return total;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, curr) => {
+    total *= curr;
+    return total;
+  })
 };
 
-const power = function() {
-	
+const power = function(b, e) {
+	return b ** e;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let total = 1
+  for (let y = 1; y <= x; y++) {
+    total *= y;
+  }
+  return total;
 };
 
 // Do not edit below this line
