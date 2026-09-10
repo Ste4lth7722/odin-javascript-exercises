7404e4bdd050e81a3262daf50fca11bcb4998069
Sam Hunt <sam@poggle.org>
2026-09-10 12:01:30 +0100

    Complete Exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    let prev = 0;
+    let curr = 1;
 
+    if (x < 0){
+        return "OOPS";
+    }
+    for(let i = 0; i < x; i++) {
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
 };
 
 // Do not edit below this line
