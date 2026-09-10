80b912a9142f2fae297ec69248428dfd6cc19d49
Sam Hunt <sam@poggle.org>
2026-09-10 11:53:43 +0100

    Complete Exercise 12

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function(input) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let cleaned = input.toLowerCase().split('').filter((char) => alphanumerical.includes(char));
+    let reversed = cleaned.slice().reverse().join('');
+    return reversed === cleaned.join('');
 };
 
 // Do not edit below this line
