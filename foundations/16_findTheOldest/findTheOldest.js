a4913126935d9a55dbc7e6692fc4bc217a802790
Sam Hunt <sam@poggle.org>
2026-09-10 12:13:14 +0100

    Complete Exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.sort((item, nextItem) => {
+        if (item.yearOfDeath === undefined) item.yearOfDeath = 2026;
+        if (nextItem.yearOfDeath === undefined) nextItem.yearOfDeath = 2026;
+        return (item.yearOfBirth - item.yearOfDeath) > (nextItem.yearOfBirth - nextItem.yearOfDeath) ? 1 : -1;
+    });
+    return arr[0]
 };
 
 // Do not edit below this line
